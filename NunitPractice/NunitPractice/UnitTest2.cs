ca3c2a323fee4604fdb92ba86c50ba2700eca74e
Shiyam Jannan <shiyamtj@gmail.com>
2022-03-11 16:19:39 +1300

    Sample Test Learning 01

---
+++ NunitPractice/NunitPractice/UnitTest2.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NunitPractice
+{
+    public class UnitTest2
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1_1()
+        {
+            Assert.Pass();
+        }
+    }
+}
