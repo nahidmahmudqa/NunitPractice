ab747ba0f1904490666923799f4a182c684f30d5
NahidMahmudQA <Nahid.mahmud.qa@gmail.com>
2022-03-11 16:48:53 +1300

    Third Test

---
+++ NunitPractice/NunitPractice/UnitTest3.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace NunitPractice
+{
+    public class UnitTest3
+    { 
+        public class Tests
+        {
+            [Test]
+            public void Test_AreEqual()
+            {
+                int m= 5;
+                Assert.AreEqual(m, 5, "M value mismatched");
+                Assert.That(m, Is.EqualTo(5), "M value mismatched");
+            }
+
+            [Test]
+            public void Test_AreNotEqual()
+            {
+                int m = 6;
+                Assert.AreNotEqual(m, 5, "M value mismatched");
+                Assert.That(m, Is.Not.EqualTo(5), "M value mismatched");
+            }
+        }
+    }
+}
